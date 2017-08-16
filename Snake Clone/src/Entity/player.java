95b3aa63675330a75210e2ef9a246184bd2a0da5
Kion-Smith <kion_smith@hotmail.com>
2017-08-15 19:06:07 -0500

    added snake class to hold position, and made the tail and body move, close to completion

--- Snake Clone/src/Entity/player.java
+++ Snake Clone/src/Entity/player.java
@@ -17,7 +17,7 @@ public class player
 	private int pPosX =0;
 	private int pPosY =0;
 	
-	private ArrayList<String> snakeLength;
+	private ArrayList<snake> snakeLength;
 	
 	private int moveSpeed = 20;
 	
@@ -25,8 +25,8 @@ public class player
 	{
 		pPosX  = x;
 		pPosY  = y;
-		snakeLength = new ArrayList<String>();
-		snakeLength.add(x+","+y);
+		snakeLength = new ArrayList<snake>();
+		snakeLength.add(new snake(x,y));
 	}
 	
 	public int currentX()
@@ -49,7 +49,7 @@ public class player
 	
 	public void addToSnake(int x, int y)
 	{
-		snakeLength.add(x+","+y);
+		snakeLength.add(new snake(x,y) );
 	}
 	public void setUp()
 	{
@@ -81,21 +81,91 @@ public class player
 	}
 	public void getNextPostion()
 	{
+		snake last = new snake(0,0);
 		if(moveUp)
 		{
-			pPosY -=moveSpeed;	
+			
+			
+			for(int i =0;i<snakeLength.size();i++)
+			{
+				
+				if(i==0)
+				{	
+					last = new snake(pPosX,pPosY);
+					pPosY -=moveSpeed;
+					snakeLength.set(i, new snake(pPosX,pPosY) );
+				}
+				else
+				{
+					snake temp = new snake(snakeLength.get(i).getX() ,snakeLength.get(i).getY());
+					snakeLength.set(i, new snake(last.getX(),last.getY()) );
+					last = new snake(temp.getX(),temp.getY());
+					
+				}
+			}
+			
 		}
 		if(moveDown)
 		{
-			pPosY +=moveSpeed;
+			
+			for(int i =0;i<snakeLength.size();i++)
+			{
+				
+				if(i==0)
+				{	
+					last = new snake(pPosX,pPosY);
+					pPosY +=moveSpeed;
+					snakeLength.set(i, new snake(pPosX,pPosY) );
+					
+				}
+				else
+				{
+					snake temp = new snake(snakeLength.get(i).getX() ,snakeLength.get(i).getY());
+					snakeLength.set(i, new snake(last.getX(),last.getY()) );
+					last = new snake(temp.getX(),temp.getY());
+				}
+			}
+			
 		}
 		if(moveRight)
 		{
-			pPosX +=moveSpeed;
+			
+			for(int i =0;i<snakeLength.size();i++)
+			{
+				
+				if(i==0)
+				{	
+					last = new snake(pPosX,pPosY);
+					pPosX +=moveSpeed;
+					snakeLength.set(i, new snake(pPosX,pPosY) );
+				}
+				else
+				{
+					snake temp = new snake(snakeLength.get(i).getX() ,snakeLength.get(i).getY());
+					snakeLength.set(i, new snake(last.getX(),last.getY()) );
+					last = new snake(temp.getX(),temp.getY());
+				}
+			}
 		}
 		if(moveLeft)
 		{
-			pPosX -=moveSpeed;
+			
+			for(int i =0;i<snakeLength.size();i++)
+			{
+				
+				if(i==0)
+				{	
+					last = new snake(pPosX,pPosY);
+					pPosX -=moveSpeed;
+					snakeLength.set(i, new snake(pPosX,pPosY) );
+				}
+				else
+				{
+					snake temp = new snake(snakeLength.get(i).getX() ,snakeLength.get(i).getY());
+					snakeLength.set(i, new snake(last.getX(),last.getY()) );
+					last = new snake(temp.getX(),temp.getY());
+				}
+			}
 		}
 	}
 	public void update()
@@ -105,6 +175,12 @@ public class player
 	public void draw(Graphics2D g)
 	{
 		g.setColor(Color.GREEN);
-		g.fillRect(pPosX, pPosY, pWidth, pHeight);
+		//g.fillRect(pPosX, pPosY, pWidth, pHeight);
+		
+		for(int i =0;i<snakeLength.size();i++)
+		{
+			
+			g.fillRect(snakeLength.get(i).getX(),snakeLength.get(i).getY(), pWidth, pHeight);
+		}
 	}
 }
---
+++ Snake Clone/src/Entity/snake.java
@@ -0,0 +1,29 @@
+package Entity;
+
+public class snake 
+{
+	private int x;
+	private int y;
+	
+	public snake(int tempx,int tempy)
+	{
+		x = tempx;
+		y = tempy;
+	}
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
+	}
+	public void setX()
+	{
+		
+	}
+	public void setY()
+	{
+		
+	}
+}
